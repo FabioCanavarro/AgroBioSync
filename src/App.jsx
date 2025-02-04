acf340802f4a96ef5ef855f92ecc505474f1c9f1
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-02-04 21:44:55 +0700

    deleted useless test-api.jsx and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './components/styles/pagestyle.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, HomePage, AboutPage, ContactPage } from './components/navbar';
 import Dashboard from './dashboard';
-import TestAPI from './test-api';
 
 
 const App = () => {
@@ -20,7 +19,6 @@ const App = () => {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/dashboard" element={<Dashboard baseUrl="/api/sensor-data" />} />
-            <Route path="/test-api" element={<TestAPI />} />
           </Routes>
         </main>
       </div>
